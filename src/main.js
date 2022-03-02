39c63941db826c40eec7cd8824fca1d202e81a96
lucasreisc1 <lucas.reis@involves.com>
2022-03-02 19:42:22 -0300

    add vue-apollo dependecy

--- src/main.js
+++ src/main.js
@@ -1,13 +1,17 @@
 import Vue from 'vue';
+import VueApollo from 'vue-apollo';
+import vuetify from '@/plugins/vuetify';
 import App from './App.vue';
 import './registerServiceWorker';
-import router from './router';
-import store from './store';
+import router from './router/router';
+import store from './store/store';
 
 Vue.config.productionTip = false;
+Vue.use(VueApollo);
 
 new Vue({
   router,
   store,
+  vuetify,
   render(h) { return h(App); },
 }).$mount('#app');
