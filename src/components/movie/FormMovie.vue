4a94b4b6252f4306036b7c3d7b9f2a27a241f290
lucasreisc1 <lucas.reis@involves.com>
2022-03-03 11:52:47 -0300

    feat(movie): add form movie

---
+++ src/components/movie/FormMovie.vue
@@ -0,0 +1,69 @@
+<template>
+  <v-form v-model="valid">
+    <v-container>
+      <v-row>
+        <v-col cols="12" md="4">
+          <v-text-field
+            v-model="movie.name"
+            :rules="nameRules"
+            label="Movie name"
+            required
+          ></v-text-field>
+        </v-col>
+
+        <v-col cols="12" md="4">
+          <v-text-field
+            v-model="movie.genre"
+            label="Genre"
+            required
+          ></v-text-field>
+        </v-col>
+      </v-row>
+
+      <v-row>
+        <v-col cols="12" md="4">
+          <v-text-field
+            v-model="movie.year"
+            label="Year"
+          ></v-text-field>
+        </v-col>
+
+          <v-col cols="12" md="4">
+            <v-text-field
+              v-model="movie.imdbRating"
+              label="Imdb Rating"
+            ></v-text-field>
+          </v-col>
+      </v-row>
+
+      <v-row>
+        <v-col cols="12" md="8">
+          <v-text-field
+            v-model="movie.duration"
+            label="Duration"
+          ></v-text-field>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-form>
+</template>
+
+<script>
+export default {
+  name: 'FormMovie',
+  data() {
+    return {
+      movie: {},
+      valid: false,
+      nameRules: [
+        (v) => !!v || 'Field is required',
+      ],
+      email: '',
+    };
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
--- src/views/movies/ManageMovieView.vue
+++ src/views/movies/ManageMovieView.vue
@@ -1,12 +1,15 @@
 <template>
   <div>
-
+    <form-movie />
   </div>
 </template>
 
 <script>
+import FormMovie from '@/components/movie/FormMovie.vue';
+
 export default {
   name: 'ManageMovieView',
+  components: { FormMovie },
 };
 </script>
 
