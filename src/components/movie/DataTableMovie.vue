6eed61637b3bfe527ef597ac2d493add75a958b8
lucasreisc1 <lucas.reis@involves.com>
2022-03-03 10:37:45 -0300

    feat(movie): add style on data table

--- src/components/movie/DataTableMovie.vue
+++ src/components/movie/DataTableMovie.vue
@@ -1,26 +1,28 @@
 <template>
-  <v-simple-table>
-    <template v-slot:default>
-      <thead>
-      <tr>
-        <th class="text-left"> Name </th>
-        <th class="text-left"> Genre </th>
-        <th class="text-left"> Imdb Rating </th>
-        <th class="text-left"> Duration </th>
-        <th class="text-left"> Year </th>
-      </tr>
-      </thead>
-      <tbody>
-      <tr v-for="item in movies" :key="item.id" >
-        <td>{{ item.name }}</td>
-        <td>{{ item.genre }}</td>
-        <td>{{ item.imdbRating }}</td>
-        <td>{{ item.duration }}</td>
-        <td>{{ item.year }}</td>
-      </tr>
-      </tbody>
-    </template>
-  </v-simple-table>
+  <v-container>
+    <v-simple-table>
+      <template v-slot:default>
+        <thead>
+        <tr>
+          <th class="text-left"> Name </th>
+          <th class="text-left"> Genre </th>
+          <th class="text-left"> Imdb Rating </th>
+          <th class="text-left"> Duration </th>
+          <th class="text-left"> Year </th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr v-for="item in movies" :key="item.id" >
+          <td>{{ item.name }}</td>
+          <td>{{ item.genre }}</td>
+          <td>{{ item.imdbRating }}</td>
+          <td>{{ item.duration }}</td>
+          <td>{{ item.year }}</td>
+        </tr>
+        </tbody>
+      </template>
+    </v-simple-table>
+  </v-container>
 </template>
 
 <script>
